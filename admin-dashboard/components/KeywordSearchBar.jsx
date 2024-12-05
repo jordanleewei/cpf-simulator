8bd32b7781000be5af0238fbff13c130a21315e9
Ethan Wong SW <ethan.wong.shao.wei@gmail.com>
2024-12-05 11:23:51 +0800

    Implement keyword searchbar for profile

---
+++ admin-dashboard/components/KeywordSearchBar.jsx
@@ -0,0 +1,18 @@
+import { IoMdSearch } from "react-icons/io";
+
+export default function KeywordSearchBar({ setSearch }) {
+  return (
+    <div className="relative">
+      <input
+        className="bg-light-gray pl-7 pr-14 rounded-md py-1 w-80"
+        type="text"
+        placeholder="Search by Keyword"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <IoMdSearch
+        size={24}
+        className="absolute text-slate-300 top-1.5 left-1"
+      />
+    </div>
+  );
+}
